5b28d7be068a33024d525c9bfb10947ecd160a9b
Auguste ALEXANDRE <augustealexandre99@gmail.com>
2026-04-22 19:39:34 +0200

    modification du message d'erreur 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,7 +5,7 @@ export function NotFound() {
   return (
     <div className="notfound-page">
       <h1 className="notfound-code">404</h1>
-      <p className="notfound-message">Cette page n'existe pas.</p>
+      <p className="notfound-message">Gustave tu pètes les couilles !!!</p>
       <Link to="/" className="notfound-link">Retour à l'accueil</Link>
     </div>
   );
