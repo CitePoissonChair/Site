5d8f229f5420da117f3a969450a60e1cc71d4b77
CitePoissonChair <citepoissonchair@gmail.com>
2026-04-25 15:46:25 +0200

    Update SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 interface SiteHeaderProps {
   title?: string;
@@ -7,18 +8,24 @@ interface SiteHeaderProps {
 }
 
 export function SiteHeader({ title, showBack = false, backTo = '/' }: SiteHeaderProps) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <div className="flex flex-col items-center pt-[4vh] pb-[2vh]">
       <Link to="/">
         <img
-          src="/images/cpc_logo.png"
+          src={hovered ? "/images/cpc_logo_hover.png" : "/images/cpc_logo.png"}
           alt="Logo Cité Poisson-Chair"
-          className="h-[12vh] mb-[1.5vh] max-[1000px]:h-[10vh]"
+          className="h-[12vh] mb-[1.5vh] max-[1000px]:h-[10vh] transition-transform duration-200 ease-in-out hover:scale-105 cursor-pointer"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
         />
       </Link>
+
       <h1 className="font-normal text-[3vh] text-[rgb(250,250,250)] whitespace-nowrap text-center max-[1000px]:text-[4vh]">
         {title ?? 'Cité Poisson-Chair'}
       </h1>
+
       {showBack && (
         <Link
           to={backTo}
