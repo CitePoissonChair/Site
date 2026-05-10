930240e743f01bfc1ad0297fa415925a7fc5afe2
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-10 16:27:58 +0200

    Update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -21,7 +21,6 @@ export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
           className="
             flex w-full h-full
             will-change-transform
-            snap-x snap-mandatory
           "
         >
           {images.map((img, i) => (
@@ -30,70 +29,49 @@ export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
               className="
                 flex-none w-screen h-full
                 relative overflow-hidden
-                snap-center
                 group
               "
             >
-              {img.link ? (
-                <Link to={img.link} className="w-full h-full block relative">
+              <Link to={img.link ?? '#'} className="w-full h-full block relative">
 
-                  {/* BACK LAYER (parallax slow) */}
+                {/* IMAGE (parallax léger) */}
+                <div className="
+                  absolute inset-0 scale-110
+                  transition-transform duration-[1200ms]
+                  group-hover:scale-125
+                ">
+                  <img
+                    src={img.src}
+                    alt={img.alt}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                {/* OVERLAY */}
+                <div className="
+                  absolute inset-0 bg-black/30
+                  group-hover:bg-black/10
+                  transition
+                " />
+
+                {/* LABEL */}
+                {img.label && (
                   <div className="
-                    absolute inset-0 scale-110
-                    transition-transform duration-[1200ms]
-                    group-hover:scale-125
+                    absolute inset-0 flex items-center justify-center
+                    pointer-events-none
                   ">
-                    <img
-                      src={img.src}
-                      alt={img.alt}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-
-                  {/* OVERLAY LAYER */}
-                  <div className="
-                    absolute inset-0 bg-black/30
-                    group-hover:bg-black/10
-                    transition
-                  " />
-
-                  {/* FOREGROUND LABEL */}
-                  {img.label && (
                     <div className="
-                      absolute inset-0 flex items-center justify-center
-                      pointer-events-none
+                      text-[7vh] font-bold text-white
+                      transition-all duration-300
+                      group-hover:text-yellow-400
+                      group-hover:scale-110
                     ">
-                      <div className="
-                        text-[7vh] font-bold text-white
-                        transition-all duration-500
-                        group-hover:text-yellow-400
-                        group-hover:scale-110
-                      ">
-                        {img.label}
-                      </div>
-                    </div>
-                  )}
-
-                </Link>
-              ) : (
-                <>
-                  <div className="absolute inset-0 scale-110 group-hover:scale-125 transition-transform duration-[1200ms]">
-                    <img
-                      src={img.src}
-                      alt={img.alt}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-
-                  <div className="absolute inset-0 bg-black/30 group-hover:bg-black/10 transition" />
-
-                  {img.label && (
-                    <div className="absolute inset-0 flex items-center justify-center text-white text-[7vh] font-bold pointer-events-none">
                       {img.label}
                     </div>
-                  )}
-                </>
-              )}
+                  </div>
+                )}
+
+              </Link>
             </div>
           ))}
         </div>
