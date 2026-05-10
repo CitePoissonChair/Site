b56fcc4c52150ed325cb6aca4ccf9dd3a2532aaa
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-10 16:58:56 +0200

    Update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 
 interface ImageCarouselProps {
@@ -15,24 +15,16 @@ export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
     return (
       <div className="w-full h-screen overflow-hidden flex-none relative">
         <div className="w-full h-full flex items-center">
-
+          
           {/* TRACK */}
           <div
             ref={ref}
-            className="
-              flex w-full h-full
-              will-change-transform
-              select-none
-            "
+            className="flex w-full h-full will-change-transform select-none"
           >
             {images.map((img, i) => (
               <div
                 key={i}
-                className="
-                  flex-none w-screen h-full
-                  flex items-center justify-center
-                  relative group
-                "
+                className="flex-none w-screen h-full flex items-center justify-center relative group"
               >
                 <div className="w-full h-full relative overflow-hidden">
 
@@ -41,30 +33,14 @@ export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
                       <img
                         src={img.src}
                         alt={img.alt}
-                        className="
-                          w-full h-full object-cover
-                          transition-transform duration-1000 ease-out
-                          group-hover:scale-110
-                        "
+                        className="w-full h-full object-cover transition-transform duration-1000 ease-out group-hover:scale-110"
                       />
 
-                      <div className="
-                        absolute inset-0
-                        bg-black/20
-                        group-hover:bg-black/10
-                        transition
-                      " />
+                      <div className="absolute inset-0 bg-black/20 group-hover:bg-black/10 transition" />
 
                       {img.label && (
-                        <div className="
-                          absolute inset-0 flex items-center justify-center
-                          pointer-events-none
-                        ">
-                          <div className="
-                            text-[7vh] font-bold text-white
-                            transition-all duration-500
-                            group-hover:text-yellow-400
-                          ">
+                        <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+                          <div className="text-[7vh] font-bold text-white transition-all duration-500 group-hover:text-yellow-400">
                             {img.label}
                           </div>
                         </div>
@@ -79,11 +55,7 @@ export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
                       />
 
                       {img.label && (
-                        <div className="
-                          absolute inset-0 flex items-center justify-center
-                          text-[7vh] font-bold text-white
-                          pointer-events-none
-                        ">
+                        <div className="absolute inset-0 flex items-center justify-center text-[7vh] font-bold text-white pointer-events-none">
                           {img.label}
                         </div>
                       )}
