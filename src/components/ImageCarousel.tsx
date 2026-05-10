8124de78955f9eb768861656aaf6fa8a5766b4ef
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-10 17:09:13 +0200

    Update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -13,59 +13,35 @@ interface ImageCarouselProps {
 export const ImageCarousel = forwardRef<HTMLDivElement, ImageCarouselProps>(
   ({ images }, ref) => {
     return (
-      <div className="w-full h-screen overflow-hidden relative">
-        
-        {/* TRACK */}
+      <div className="w-full h-screen overflow-hidden">
         <div
           ref={ref}
-          className="
-            flex h-full w-full
-            will-change-transform
-            select-none
-          "
+          className="flex w-full h-full will-change-transform select-none"
         >
           {images.map((img, i) => (
-            <div
-              key={i}
-              className="flex-none w-screen h-full relative"
-            >
-              <div className="w-full h-full relative">
-
-                {img.link ? (
-                  <Link to={img.link} className="w-full h-full block">
-                    <img
-                      src={img.src}
-                      alt={img.alt}
-                      className="w-full h-full object-cover"
-                    />
-
-                    {img.label && (
-                      <div className="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
-                        {img.label}
-                      </div>
-                    )}
-                  </Link>
-                ) : (
-                  <>
-                    <img
-                      src={img.src}
-                      alt={img.alt}
-                      className="w-full h-full object-cover"
-                    />
-
-                    {img.label && (
-                      <div className="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
-                        {img.label}
-                      </div>
-                    )}
-                  </>
-                )}
-
-              </div>
+            <div key={i} className="flex-none w-screen h-full relative">
+              {img.link ? (
+                <Link to={img.link} className="w-full h-full block">
+                  <img src={img.src} className="w-full h-full object-cover" />
+                  {img.label && (
+                    <div className="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
+                      {img.label}
+                    </div>
+                  )}
+                </Link>
+              ) : (
+                <>
+                  <img src={img.src} className="w-full h-full object-cover" />
+                  {img.label && (
+                    <div className="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
+                      {img.label}
+                    </div>
+                  )}
+                </>
+              )}
             </div>
           ))}
         </div>
-
       </div>
     );
   }
