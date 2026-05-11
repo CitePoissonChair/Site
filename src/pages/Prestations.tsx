e8103438e294ca77a9838f340ce5fe917ba6087b
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-11 11:00:31 +0200

    Update Prestations.tsx

--- src/pages/Prestations.tsx
+++ src/pages/Prestations.tsx
@@ -19,7 +19,7 @@ const livesImages = [
 ];
 
 const clipsImages = [
-  { src: '/prestationscontenu/Clip father of sins.mp4', alt: 'Clips 1', label: 'Clips', link: '/clips' },
+  { src: '/prestationscontenu/Clip father of sins.gif', alt: 'Clips 1', label: 'Clips', link: '/clips' },
   { src: '/images/Ecrits/Station Soleil Bleu/Contenus/CPC_station_soleil_bleu_1.jpg', alt: 'Clips 2', link: '/clips' },
   { src: '/images/Ecrits/Station Soleil Bleu/Contenus/CPC_station_soleil_bleu_2.jpg', alt: 'Clips 3', link: '/clips' },
 ];
