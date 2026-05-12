9d7c918af737fe2aacc58406a5dc45bbb8886499
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-12 16:14:37 +0200

    Update Photos.tsx

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -22,52 +22,60 @@ export function Photos() {
   }, []);
 
   return (
-    <div className="h-screen w-screen bg-black text-white overflow-hidden">
-      
-      {/* header */}
-      <div className="absolute top-0 left-0 w-full z-20">
+    <div className="h-screen w-screen bg-black text-white flex flex-col overflow-hidden">
+
+      {/* HEADER (fixe, toujours visible) */}
+      <div className="shrink-0 z-20">
         <div className="flex justify-center py-[3vh]">
           <SiteHeader title="Photos" showBack backTo="/prestations" />
         </div>
       </div>
 
-      {/* IMPORTANT: scroll container */}
-      <div
-        className="
-          flex
-          h-full
-          w-max
-          overflow-x-auto
-          overflow-y-hidden
-          snap-x
-          snap-mandatory
-        "
-        style={{
-          scrollBehavior: 'smooth',
-        }}
-      >
-        {photos.map((photo, i) => (
-          <div
-            key={i}
-            className="
-              w-screen
-              h-screen
-              flex
-              items-center
-              justify-center
-              snap-center
-              shrink-0
-              relative
-            "
-          >
-            <img
-              src={photo}
-              className="w-full h-full object-cover"
-            />
+      {/* CAROUSEL AREA */}
+      <div className="flex-1 overflow-hidden">
 
-            <div className="absolute inset-0 bg-black/30" />
-          </div>
-        ))}
+        {/* SCROLLER */}
+        <div
+          className="
+            flex
+            h-full
+            w-full
+            overflow-x-auto
+            overflow-y-hidden
+            snap-x
+            snap-mandatory
+          "
+        >
+          {photos.map((photo, i) => (
+            <div
+              key={i}
+              className="
+                min-w-full
+                h-full
+                flex
+                items-center
+                justify-center
+                snap-center
+                shrink-0
+                relative
+              "
+            >
+              {/* IMAGE (NOT fullscreen now, contains nicely) */}
+              <img
+                src={photo}
+                className="
+                  max-h-[80%]
+                  max-w-[90%]
+                  object-cover
+                  rounded-[2vh]
+                "
+              />
+
+              {/* overlay léger */}
+              <div className="absolute inset-0 bg-black/20 pointer-events-none" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
