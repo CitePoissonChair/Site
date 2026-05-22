f13c22e3aa24edf2ef43762d5172e46deef1af82
CitePoissonChair <citepoissonchair@gmail.com>
2026-05-22 12:43:22 +0200

    Update ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -23,8 +23,18 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
           className="
             w-full h-full
             flex items-center
-            overflow-x-auto overflow-y-hidden
+
+            overflow-x-auto
+            overflow-y-hidden
+
             no-scrollbar
+
+            snap-x
+            snap-mandatory
+
+            touch-pan-x
+
+            [-webkit-overflow-scrolling:touch]
           "
         >
 
@@ -49,6 +59,7 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
                   relative
                   group
                   overflow-hidden
+                  snap-center
                 "
               >
 
@@ -63,11 +74,11 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
                     "
                   >
 
-                    {/* IMAGE */}
                     <img
                       src={image.src}
                       alt={image.alt}
                       loading="lazy"
+                      draggable={false}
                       className="
                         absolute inset-0
                         w-full h-full
@@ -81,23 +92,22 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
                         ease-out
 
                         will-change-transform
+
+                        pointer-events-none
+                        select-none
                       "
                     />
 
-                    {/* OVERLAY */}
                     <div
                       className="
                         absolute inset-0
                         bg-black/30
-
                         group-hover:bg-black/15
-
                         transition-all
                         duration-700
                       "
                     />
 
-                    {/* LABEL */}
                     {image.label && (
                       <div
                         className="
@@ -148,8 +158,6 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
                                 text-[7vh]
                                 font-bold
                                 tracking-wide
-
-                                group-hover:text-yellow-400
                               `
                               : ''
                             }
@@ -157,115 +165,16 @@ export const ImageCarousel = forwardRef<HTMLDivElement, Props>(
                         >
                           {image.label}
                         </div>
+
                       </div>
                     )}
 
                   </Link>
-                ) : (
-                  <>
-                    {/* IMAGE */}
-                    <img
-                      src={image.src}
-                      alt={image.alt}
-                      loading="lazy"
-                      className="
-                        absolute inset-0
-                        w-full h-full
-                        object-cover object-center
-
-                        scale-100
-                        group-hover:scale-110
-
-                        transition-transform
-                        duration-[1600ms]
-                        ease-out
-
-                        will-change-transform
-                      "
-                    />
-
-                    {/* OVERLAY */}
-                    <div
-                      className="
-                        absolute inset-0
-                        bg-black/30
-
-                        group-hover:bg-black/15
-
-                        transition-all
-                        duration-700
-                      "
-                    />
-
-                    {/* LABEL */}
-                    {image.label && (
-                      <div
-                        className="
-                          absolute inset-0
-                          flex items-center justify-center
-                          pointer-events-none
-                          p-8 md:p-14
-                        "
-                      >
-
-                        <div
-                          className={`
-                            whitespace-pre-line
-                            text-white
-                            text-center
-
-                            transition-all
-                            duration-500
-                            ease-out
-
-                            group-hover:scale-105
-
-                            ${image.labelType === 'list'
-                              ? `
-                                text-[11px]
-                                md:text-sm
-                                uppercase
-                                tracking-[0.35em]
-                                leading-[1.9]
-                                font-light
-                              `
-                              : ''
-                            }
-
-                            ${image.labelType === 'quote'
-                              ? `
-                                text-2xl
-                                md:text-5xl
-                                leading-tight
-                                font-light
-                                max-w-[700px]
-                              `
-                              : ''
-                            }
-
-                            ${!image.labelType
-                              ? `
-                                text-[7vh]
-                                font-bold
-                                tracking-wide
-
-                                group-hover:text-yellow-400
-                              `
-                              : ''
-                            }
-                          `}
-                        >
-                          {image.label}
-                        </div>
-                      </div>
-                    )}
-                  </>
-                )}
+                ) : null}
 
               </div>
             ))}
 
-            {/* SPACING FIN */}
             <div className="flex-none w-[50px] h-full" />
 
           </div>
